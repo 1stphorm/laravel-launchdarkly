19779d804f26aa8f9b1e624b078bf68485355519
Nick Vahalik <nvahalik@1stphorm.com>
2024-03-14 18:15:52 -0500

    LDContext instead of LDUser.

--- src/Facades/LaunchDarkly.php
+++ src/Facades/LaunchDarkly.php
@@ -8,14 +8,14 @@ use Ocus\LaravelLaunchDarkly\Integrations\InMemory;
 use Ocus\LaravelLaunchDarkly\Services\LaravelLaunchDarklyInMemoryService;
 
 /**
- * @method static mixed variation(string $key, \LaunchDarkly\LDUser $user, $default = false)
- * @method static \LaunchDarkly\EvaluationDetail variationDetail(string $key, \LaunchDarkly\LDUser $user, $default = false)
+ * @method static mixed variation(string $key, \LaunchDarkly\LDContext $user, $default = false)
+ * @method static \LaunchDarkly\EvaluationDetail variationDetail(string $key, \LaunchDarkly\LDContext $user, $default = false)
  * @method static bool isOffline()
- * @method static void track(string $eventName, \LaunchDarkly\LDUser $user, $data = null, $metricValue = null)
- * @method static void identify(\LaunchDarkly\LDUser $user)
- * @method static \LaunchDarkly\FeatureFlagsState allFlagsState(\LaunchDarkly\LDUser $user, array $options = [])
- * @method static void alias(\LaunchDarkly\LDUser $user, \LaunchDarkly\LDUser $previousUser)
- * @method static string secureModeHash(\LaunchDarkly\LDUser $user)
+ * @method static void track(string $eventName, \LaunchDarkly\LDContext $user, $data = null, $metricValue = null)
+ * @method static void identify(\LaunchDarkly\LDContext $user)
+ * @method static \LaunchDarkly\FeatureFlagsState allFlagsState(\LaunchDarkly\LDContext $user, array $options = [])
+ * @method static void alias(\LaunchDarkly\LDContext $user, \LaunchDarkly\LDContext $previousUser)
+ * @method static string secureModeHash(\LaunchDarkly\LDContext $user)
  * @method static bool flush()
  * @method static void addFlag(array $flag)
  * @method static void addFlagValue(string $key, $value)
